93f4a81b2a0573226e40f1c3aff83b313fb6259e
Michael Aroworade <Ayoaro85@gmail.com>
2025-03-21 00:23:25 +0100

    import

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+# Write a program that imports the function def add(a, b):
+# from the file add_0.py and prints the result of the addition 1 + 2 = 3
+
+from add_0 import add
+
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
